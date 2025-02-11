68e6dc13338e0bc9d08f0af3dfe66d46efbf0e4e
Maksym Seliutin <maxwell7k@gmail.com>
2025-02-11 10:27:17 +0200

    nextcfg fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
-import "../styles/globals.css";
+import "@/styles/globals.css";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import { TodoForm } from '@/Components/TodoForm/TodoForm';
-import './globals.css';
 export default function Home() {
 	return (
       <div className='p-7 bg-stone-200/30 w-screen h-screen'>

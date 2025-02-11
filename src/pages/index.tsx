4a7e3cdfa102572970d022e99300998281892be2
Maksym Seliutin <maxwell7k@gmail.com>
2025-02-11 10:13:46 +0200

    nextcfg fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { TodoForm } from '@/Components/TodoForm/TodoForm';
-
+import './globals.css';
 export default function Home() {
 	return (
       <div className='p-7 bg-stone-200/30 w-screen h-screen'>

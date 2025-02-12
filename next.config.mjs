df61df1b382cecb8ca5ca6344e5f65991804c0d1
Maksym Seliutin <maxwell7k@gmail.com>
2025-02-12 17:07:55 +0200

    fix: fixed path

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ const nextConfig = {
   /* config options here */
   reactStrictMode: true,
   output: "export",
-  basePath: "/next-test",
-  assetPrefix: "/next-test/",
+  basePath: "/todo-app-veel",
+  assetPrefix: "/todo-app-veel/",
   trailingSlash: true,
   env: {
     NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY || "https://jsonplaceholder.typicode.com/todos",
